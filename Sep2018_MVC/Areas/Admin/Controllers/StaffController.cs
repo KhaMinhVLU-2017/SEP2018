e640fc130d6bfc0fdfc91cd83f91b57a8d304ed5
KhaMinhVLU-2017 <minhnguyengit@gmail.com>
2018-05-10 14:31:15 +0700

    Complete Backend Create Section on Views and Action

--- Sep2018_MVC/Areas/Admin/Controllers/StaffController.cs
+++ Sep2018_MVC/Areas/Admin/Controllers/StaffController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using Sep2018_MVC;
+using Sep2018_MVC.Models;
 
 namespace Sep2018_MVC.Areas.Admin.Controllers
 {
@@ -19,6 +22,17 @@ namespace Sep2018_MVC.Areas.Admin.Controllers
         }
         public ActionResult CreateSection()
         {
+            SEP_2018_T6Entities db = new SEP_2018_T6Entities();
+            List<Course> meoCourse = new List<Course>();
+            meoCourse = db.Courses.ToList();
+            List<Class> meoClass = new List<Class>();
+            meoClass =  db.Classes.ToList();
+            List<Subject> meoSubject = new List<Subject>();
+            meoSubject = db.Subjects.ToList();
+
+            ViewData["meoCourse"] = meoCourse;
+            ViewData["meoClass"] = meoClass;
+            ViewData["meoSubject"] = meoSubject;
             return View();
         }
     }
