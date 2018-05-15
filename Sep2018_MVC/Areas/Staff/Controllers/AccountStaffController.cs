5f02858985fefe09babe8d4878f78d41df3f1ac1
Khai Dang <quangkhai897@gmail.com>
2018-05-16 01:16:53 +0700

    View Login and forget password for staff

---
+++ Sep2018_MVC/Areas/Staff/Controllers/AccountStaffController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sep2018_MVC.Areas.Staff.Controllers
+{
+    public class AccountStaffController : Controller
+    {
+        // GET: Staff/AccountStaff
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
