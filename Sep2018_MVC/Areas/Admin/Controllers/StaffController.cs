aa6681a00fb2a0f3b1a9dec958f513df11aefeb3
KhaMinhVLU-2017 <minhnguyengit@gmail.com>
2018-05-15 15:01:11 +0700

    Complete Ajax From CreateSection

--- Sep2018_MVC/Areas/Admin/Controllers/StaffController.cs
+++ Sep2018_MVC/Areas/Admin/Controllers/StaffController.cs
@@ -28,7 +28,6 @@ namespace Sep2018_MVC.Areas.Admin.Controllers
            
             List<Course> meoCourse = new List<Course>();
             meoCourse = db.Courses.ToList();
-
             return View(meoCourse);
         }
         public ActionResult CreateAccount()
@@ -36,12 +35,33 @@ namespace Sep2018_MVC.Areas.Admin.Controllers
             return View();
         }
 
-        public ActionResult ClassMeo(int ?id)
+        public ActionResult ClassMeo(int? id,int? id_course)
         {
             List<object> meo = new List<object>();
-            foreach(var item in db.Classes.Where(s=>s.FK_Course==id))
+            foreach (var item in db.Learnings.Where(s=>s.FK_Semester==id && s.Class.FK_Course==id_course))
             {
-                meo.Add(new { id_class= item.id, classname = item.ClassName});
+                meo.Add(new { id_class = item.Class.id, classname = item.Class.ClassName });
+            }
+            return Json(meo, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult SemesterMeo(int? id)
+        {
+            List<object> meo = new List<object>();
+            foreach (var item in db.Schedules.Where(s=> s.FK_Course==id))
+            {
+                meo.Add(new { id_semes = item.Semester.id, semesName = item.Semester.SemesterName });
+            }
+            return Json(meo, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Subject(int? id_Class,int? id_semester)
+        {
+            List<object> meo = new List<object>();
+            var listSubject = db.Learnings.Where(s => s.FK_Class == id_Class & s.FK_Semester== id_semester);
+            foreach(var item in listSubject)
+            {
+
+                meo.Add(new { id_subject = item.Subject.id, subName=item.Subject.SubjectName });
+              
             }
             return Json(meo, JsonRequestBehavior.AllowGet);
         }
