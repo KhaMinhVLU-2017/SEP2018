3ce504fdc13602abfd0539d9abc5c2c5c43669c3
Khai Dang <quangkhai897@gmail.com>
2018-05-18 01:39:36 +0700

    Update view login and View Layout Admin anh view login admin

---
+++ Sep2018_MVC/Areas/Admin/Controllers/ManagementController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sep2018_MVC.Areas.Admin.Controllers
+{
+    public class ManagementController : Controller
+    {
+        // GET: Admin/Management
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DemoLogin()
+        {
+            return View();
+        }
+
+
+        public ActionResult LoginAdmin()
+        {
+            return View();
+        }
+
+        public ActionResult HomeAdmin()
+        {
+            return View();
+        }
+    }
+}
