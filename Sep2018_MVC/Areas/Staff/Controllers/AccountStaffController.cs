fa19c5ea97da3cbb8f5290016c9ccbb4924346f4
TranVanXil <tranvanxil69@gmail.com>
2018-05-22 23:44:44 +0700

    link view con va add view Schedule

--- Sep2018_MVC/Areas/Staff/Controllers/AccountStaffController.cs
+++ Sep2018_MVC/Areas/Staff/Controllers/AccountStaffController.cs
@@ -14,34 +14,27 @@ namespace Sep2018_MVC.Areas.Staff.Controllers
             return View();
         }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 458194ba99d633145a08f91d64038d605462b09a
         public  ActionResult ChangePassword()
         {
             return View();
         }
 
-<<<<<<< HEAD
-=======
         public ActionResult CreateAccount()
         {
             return View();
         }
-<<<<<<< HEAD
+
         public ActionResult Report()
         {
             return View();
         }
-=======
+
         public ActionResult InformationAccountStaff()
         {
             return View();
         }
 
->>>>>>> 458194ba99d633145a08f91d64038d605462b09a
->>>>>>> 8ab08f216a39758f6e4052cb761f5c0ec88ecbf9
+
     }
 
 }
