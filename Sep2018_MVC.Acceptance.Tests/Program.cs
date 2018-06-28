ac19f84c2f2e4b4c5a598a67f847c6b376a8c531
KhaMinhVLU-2017 <minhnguyengit@gmail.com>
2018-06-28 15:15:15 +0700

    Acceptance Test

---
+++ Sep2018_MVC.Acceptance.Tests/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sep2018_MVC.Acceptance.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
